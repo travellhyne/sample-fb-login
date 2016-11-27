63bfb3f1afd7872fe4aafd4bd3faacb5e2b7187e
Neil John Ramal <ramalneiljohn@gmail.com>
2016-11-27 10:59:05 +0800

    Remove Facebook table

--- app/FacebookAccountService.php
+++ app/FacebookAccountService.php
@@ -7,28 +7,20 @@ use Laravel\Socialite\Contracts\User as FacebookUser;
 class FacebookAccountService {
   public function createOrGetUser(FacebookUser $facebookUser)
   {
-    $fb_account = FacebookAccount::where('facebook_user_id', $facebookUser->getId())
+    $user = User::where('facebook_user_id', $facebookUser->getId())
       ->first();
 
-    if ($fb_account) {
-      return $fb_account->user;
+    if ($user) {
+      return $user;
     } else {
-      $fb_account = new FacebookAccount([
+      $user = User::create([
+        'email' => $facebookUser->getEmail(),
+        'name' => $facebookUser->getName(),
+        'password' => bcrypt($facebookUser->getName()),
+        'email_sent' => false,
         'facebook_user_id' => $facebookUser->getId(),
       ]);
-
-      $user = User::where('email', $facebookUser->getEmail())->first();
-
-      if (!$user) {
-        $user = User::create([
-          'email' => $facebookUser->getEmail(),
-          'name' => $facebookUser->getName(),
-          'password' => bcrypt($facebookUser->getEmail()),
-        ]);
-      }
-
-      $fb_account->user()->associate($user);
-      $fb_account->save();
+      $user->save();
       return $user;
     }
   }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique()->nullabel();
+            $table->string('email')->unique()->nullable();
+            $table->string('facebook_user_id')->unique();
             $table->string('password');
+            $table->boolean('email_sent');
             $table->rememberToken();
             $table->timestamps();
         });
